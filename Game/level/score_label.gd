809a9cf529bd69abc99c73af5b285c28657f4f1e
ZacDoesCodingIGuess <zaccrawfordkite@gmail.com>
2025-04-13 15:52:41 +0100

    font added
    added a retro font to the scores

--- Game/level/score_label.gd
+++ Game/level/score_label.gd
@@ -1,14 +1,15 @@
 extends Label
-@onready var player: CharacterBody2D = $"../Player"
-var score = 00.0
-#filler for when i have access to score
+
+@onready var player: CharacterBody2D = %Player
+
+@onready var test_level: Node2D = $".."
+
 
 
 func _ready() -> void:
-	text = str(int(score)) # Replace with function body.
+	text = str(test_level.score) # Replace with function body.
 
 
 
 func _process(delta: float) -> void:
-	score += 1.0 / 60
-	text = str(int(score))
+	text = str(test_level.score)
