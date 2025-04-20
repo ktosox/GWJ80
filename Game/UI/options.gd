2d51e6d3dc8f706464363f6e27eea93282fe518f
Ktosox <ktoso3@gmail.com>
2025-04-20 18:13:07 +0200

    changed how pausing works

--- Game/UI/options.gd
+++ Game/UI/options.gd
@@ -3,12 +3,12 @@ extends CanvasLayer
 
 
 func _ready() -> void:
-	Engine.time_scale = 0.0
+	get_tree().paused = true
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("options access"):
 		
-		Engine.time_scale = 1.0
+		get_tree().paused = false
 		queue_free() # Replace with function body.
 
 
--- Game/UI/tutorial/tutorial.gd
+++ Game/UI/tutorial/tutorial.gd
@@ -3,12 +3,13 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Engine.time_scale = 0.0
+	get_tree().paused = true
+	#Engine.time_scale = 0.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("tutorial_exit")):
-		Engine.time_scale = 1.0
+		get_tree().paused = false
 		GameManager.isInTutorial = false
 		queue_free()
