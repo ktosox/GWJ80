c537888ac64fcd320138cfb3456ef8ff99cbfcf8
Ktosox <ktoso3@gmail.com>
2025-04-20 11:32:10 +0200

    added a grab SFX

--- Game/player/grabby_player_gun.gd
+++ Game/player/grabby_player_gun.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var bullet_speed = 800
 
+signal grab
 
 var grabbed_bullet : RigidBody2D
 
@@ -11,20 +12,20 @@ func _input(event: InputEvent) -> void:
 
 func shoot():
 	if grabbed_bullet != null:
-		print("pew")
+		
 		grabbed_bullet.call_deferred("reparent",get_tree().current_scene)
 		grabbed_bullet.linear_velocity = bullet_speed * Vector2.RIGHT.rotated(global_rotation)
 		grabbed_bullet.set_deferred("freeze",false)
 		grabbed_bullet = null
 		$GrabPoint/SuckInBullets.gravity_space_override = Area2D.SPACE_OVERRIDE_COMBINE
 	else:
-		print("nope")
+
 		# a "no ammo" sound could go here
 		pass
 
 
 func _on_lock_in_bullet_body_entered(body: RigidBody2D) -> void:
-
+	emit_signal("grab")
 	body.set_collision_layer_value(6,false)
 	body.set_collision_layer_value(5,true)
 	body.modulate = Color("Cyan")
