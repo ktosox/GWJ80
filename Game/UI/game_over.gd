d3e2ea8fd588312361992e87938559819e7bf0d4
Positron Games <sharrvathgopi@gmail.com>
2025-04-20 21:32:54 +0530

    home page

--- Game/UI/game_over.gd
+++ Game/UI/game_over.gd
@@ -5,7 +5,7 @@ extends ColorRect
 func _ready() -> void:
 	get_tree().paused = true
 	animation_player.play("flashing")
-
+	$Label3/AnimationPlayer.play("flashing")
 
 func _on_reset_pressed() -> void:
 	pass
@@ -14,3 +14,5 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("up"):
 		get_tree().paused = false
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("down"):
+		get_tree().change_scene_to_file("res://level/home.tscn")
