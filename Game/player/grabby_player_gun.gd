44854300534925055624046b5c2362912659f057
Ktosox <ktoso3@gmail.com>
2025-04-21 13:10:01 +0200

    fixed an error in pickup, also made the gun feel better

--- Game/player/grabby_player_gun.gd
+++ Game/player/grabby_player_gun.gd
@@ -12,11 +12,12 @@ func _input(event: InputEvent) -> void:
 
 func shoot():
 	if grabbed_bullet != null:
-		
+		$GrabPoint.rotation = 0
 		grabbed_bullet.call_deferred("reparent",get_tree().current_scene)
 		grabbed_bullet.linear_velocity = bullet_speed * Vector2.RIGHT.rotated(global_rotation)
 		grabbed_bullet.set_deferred("freeze",false)
 		grabbed_bullet = null
+		$GrabPoint/GrabAniamtor.play("fire")
 		$GrabPoint/SuckInBullets.gravity_space_override = Area2D.SPACE_OVERRIDE_COMBINE
 	else:
 
@@ -34,11 +35,11 @@ func _on_lock_in_bullet_body_entered(body: RigidBody2D) -> void:
 	$GrabPoint.global_rotation = body.linear_velocity.angle()
 	body.linear_velocity = Vector2.ZERO
 	body.set_deferred("freeze",true)
-	body.global_position = $GrabPoint.global_position
-	body.call_deferred("reparent",$GrabPoint)
+	body.set_deferred("global_position",$GrabPoint/VisualPart.global_position)
+	body.call_deferred("reparent",$GrabPoint/VisualPart)
 	
 	grabbed_bullet = body
 	
-	$GrabPoint/GrabAniamtor.call_deferred("play","new_animation")
+	$GrabPoint/GrabAniamtor.call_deferred("play","grab")
 	$GrabPoint/SuckInBullets.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
 	pass # Replace with function body.
