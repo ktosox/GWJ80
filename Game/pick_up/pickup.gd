6f688ea41842a4136c37bf2a3573eb5abbeb7af2
SusieCatto <1susiecat1@gmail.com>
2025-04-21 11:29:21 +0200

    added more sounds

--- Game/pick_up/pickup.gd
+++ Game/pick_up/pickup.gd
@@ -4,14 +4,29 @@ enum Pickup{health = 0, shield, speed, screen_cleaner, gravity_fx}
 @export var pickup_type : Pickup
 @export var randomize_pickup = true
 var tag = "pick_up"
+signal picked_up
 
 
-
-const PLUS_HEALTH_PICKUP = preload("res://pick_up/plus_health_pickup.png")
-const SHIELD_PICKUP = preload("res://pick_up/shield_pickup.png")
-const SPEED = preload("res://pick_up/speed.png")
-const CLEANER_PICKUP = preload("res://pick_up/cleaner_pickup.png")
-const GRAVITY_FX = preload("res://pick_up/gravity_fx.png")
+const PLUS_HEALTH_PICKUP = {
+	"image" : preload("res://pick_up/plus_health_pickup.png"),
+	"sound" : preload("res://pick_up/bonus_health.wav"),
+}
+const SHIELD_PICKUP = {
+	"image" : preload("res://pick_up/shield_pickup.png"),
+	"sound" : preload("res://pick_up/shield.wav"),
+}
+const SPEED = {
+	"image" : preload("res://pick_up/speed.png"),
+	"sound" : preload("res://pick_up/bonus_speed.wav"),
+}
+const CLEANER_PICKUP = {
+	"image" : preload("res://pick_up/cleaner_pickup.png"),
+	"sound" : preload("res://pick_up/clear_screen.wav"),
+}
+const GRAVITY_FX = {
+	"image" : preload("res://pick_up/gravity_fx.png"),
+	"sound" : preload("res://pick_up/repel_bullets.wav"),
+}
 
 @onready var player = get_parent().get_node("Player")
 # Called when the node enters the scene tree for the first time.
@@ -19,26 +34,32 @@ func _ready() -> void:
 	if randomize_pickup:
 		pickup_type = randi_range(0,Pickup.size())
 	if(pickup_type == Pickup.health):
-		$CollisionShape2D/Sprite2D.texture = PLUS_HEALTH_PICKUP
+		$CollisionShape2D/Sprite2D.texture = PLUS_HEALTH_PICKUP["image"]
+		$AudioComponent.audio = PLUS_HEALTH_PICKUP["sound"]
 		#$CollisionShape2D/Sprite2D.position = Vector2(-8, 3) no idea why this line is here so I removed it for testing
 	elif(pickup_type == Pickup.shield):
 
-		$CollisionShape2D/Sprite2D.texture = SHIELD_PICKUP
+		$CollisionShape2D/Sprite2D.texture = SHIELD_PICKUP["image"]
+		$AudioComponent.audio = SHIELD_PICKUP["sound"]
 	elif(pickup_type == Pickup.speed):
 
-		$CollisionShape2D/Sprite2D.texture = SPEED
+		$CollisionShape2D/Sprite2D.texture = SPEED["image"]
+		$AudioComponent.audio = SPEED["sound"]
 	elif(pickup_type == Pickup.screen_cleaner):
 
-		$CollisionShape2D/Sprite2D.texture = CLEANER_PICKUP
+		$CollisionShape2D/Sprite2D.texture = CLEANER_PICKUP["image"]
+		$AudioComponent.audio = CLEANER_PICKUP["sound"]
 	elif(pickup_type == Pickup.gravity_fx):
 
-		$CollisionShape2D/Sprite2D.texture = GRAVITY_FX
+		$CollisionShape2D/Sprite2D.texture = GRAVITY_FX["image"]
+		$AudioComponent.audio = GRAVITY_FX["sound"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
+	picked_up.emit()
 	if(body == player):
 		if(pickup_type == Pickup.health):
 			GameManager.enable_pick_up("health")
@@ -56,4 +77,4 @@ func _on_body_entered(body: Node2D) -> void:
 		
 
 func _on_area_entered(area: Area2D) -> void:
-	print(area) # Replace with function body.
+	print_debug(area) # Replace with function body.
