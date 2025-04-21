f903d3f6dd508f93af9d2fc85603ca31580e88ed
Positron Games <sharrvathgopi@gmail.com>
2025-04-21 15:44:01 +0530

    screen_cleaner_bug_fix

--- Game/level/test_level.gd
+++ Game/level/test_level.gd
@@ -47,6 +47,6 @@ func _on_spawn_timer_timeout() -> void:
 		pickup_enemy_count = 0
 		
 func clear_enemies():
-	GameManager.current_score += $enemy_group_node.get_child_count()
+	GameManager.change_score($enemy_group_node.get_child_count())
 	for i in $enemy_group_node.get_children():
 		i.queue_free()
